ed8c1894a03bbb6f58cb17406774a32046a09d42
Zach Norman <zachnorman02@gmail.com>
2025-12-05 14:09:20 -0500

    change prefix

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=":", intents=intents)
 
 
 # ============================================================================
